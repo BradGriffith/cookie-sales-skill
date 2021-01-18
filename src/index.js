1cf230a024af837eedfac1192ca8d45cd6cb5691
Brad Griffith <brad@buckeyeinteractive.com>
2021-01-18 06:51:50 +0000

    Add Alexa Verifier Middleware and self-signed SSL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const { ExpressJS, Lambda, Webhook } = require('jovo-framework');
+const { ExpressJS, WebhookVerified: Webhook } = require('jovo-framework');
 const { app } = require('./app.js');
+const fs = require('fs');
 
 // ------------------------------------------------------------------
 // HOST CONFIGURATION
@@ -12,11 +13,16 @@ if (process.argv.indexOf('--webhook') > -1) {
   const port = process.env.JOVO_PORT || 3000;
   Webhook.jovoApp = app;
 
+  Webhook.ssl = {
+    key: fs.readFileSync('/home/ubuntu/key.pem'),
+    cert: fs.readFileSync('/home/ubuntu/cert.pem'),
+  };
+
   Webhook.listen(port, () => {
     console.info(`Local server listening on port ${port}.`);
   });
 
-  Webhook.post('/webhook', async (req, res) => {
+  Webhook.post(['/webhook','/webhook_alexa'], async (req, res) => {
     await app.handle(new ExpressJS(req, res));
   });
 }
